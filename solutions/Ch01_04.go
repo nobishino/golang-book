9089fa1dc3f65eca9fb6d6d181a8c6c02942739d
nobishino <nobishino@example.com>
2020-01-08 21:31:50 +0900

    Added sample code

---
+++ solutions/Ch01_04.go
@@ -0,0 +1,21 @@
+package solutions
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func dup1() {
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+	//Ignoring error caused by input.Err()
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
