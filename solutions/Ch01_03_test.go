f4be482935dcf9ce40c19b0574df3cbaa1a37867
nobishino <nobishino@example.com>
2020-01-09 20:50:01 +0900

    Update Benchmark of Join functions

--- solutions/Ch01_03_test.go
+++ solutions/Ch01_03_test.go
@@ -21,12 +21,6 @@ func TestNaiveJoin(t *testing.T) {
 	}
 }
 
-func BenchmarkNaiveJoin(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		NaiveJoin(massiveWords)
-	}
-}
-
 func ExampleEfficientJoin() {
 	result := EfficientJoin([]string{"hello", "world"})
 	fmt.Println(result)
@@ -40,169 +34,63 @@ func TestEfficientJoin(t *testing.T) {
 	}
 }
 
-func BenchmarkEfficientJoin(b *testing.B) {
+func BenchmarkNaiveJoinWith10Words(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		EfficientJoin(massiveWords)
+		NaiveJoin(Words10)
+	}
+}
+func BenchmarkNaiveJoinWith100Words(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		NaiveJoin(Words100)
+	}
+}
+func BenchmarkNaiveJoinWith1000Words(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		NaiveJoin(Words1000)
 	}
 }
 
-var massiveWords = []string{
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
-	"word",
+func BenchmarkNaiveJoinWith10000Words(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		NaiveJoin(Words10000)
+	}
+}
+
+func BenchmarkEfficientJoinWith10Words(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		EfficientJoin(Words10)
+	}
+}
+func BenchmarkEfficientJoinWith100Words(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		EfficientJoin(Words100)
+	}
+}
+func BenchmarkEfficientJoinWith1000Words(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		EfficientJoin(Words1000)
+	}
+}
+func BenchmarkEfficientJoinWith10000Words(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		EfficientJoin(Words10000)
+	}
+}
+
+var Words10 = make([]string, 10)
+var Words100 = make([]string, 100)
+var Words1000 = make([]string, 1000)
+var Words10000 = make([]string, 10000)
+
+func init() {
+	word := "word"
+	for i := 0; i < 10; i++ {
+		Words10[i] = word
+	}
+	for i := 0; i < 100; i++ {
+		Words100[i] = word
+	}
+	for i := 0; i < 1000; i++ {
+		Words1000[i] = word
+	}
 }
