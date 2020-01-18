dc11a4796ffb57f5cc2d207828a183d0c1f895f7
nobishino <nobishino@example.com>
2020-01-18 15:11:38 +0900

    Problem 1.4の誤読バージョン

--- main.go
+++ main.go
@@ -3,5 +3,6 @@ package main
 import "github.com/nobishino/golang-book/solutions"
 
 func main() {
-	solutions.Dup2()
+	// solutions.Dup2()
+	solutions.Dup_Godoku()
 }
--- solutions/Ch01_04.go
+++ solutions/Ch01_04.go
@@ -41,7 +41,7 @@ type inclusion struct {
 func countLines(f *os.File, counts map[string]*inclusion) {
 	input := bufio.NewScanner(f)
 	for input.Scan() {
-		fmt.Println(input.Text(), f.Name())
+		// fmt.Println(input.Text(), f.Name())
 		if s := counts[input.Text()]; s == nil {
 			counts[input.Text()] = &inclusion{Count: 1, Files: make(map[string]int)}
 			counts[input.Text()].Files[f.Name()]++
@@ -50,4 +50,42 @@ func countLines(f *os.File, counts map[string]*inclusion) {
 			counts[input.Text()].Files[f.Name()]++
 		}
 	}
+}
+
+func Dup_Godoku() {
+	counts := make(map[string]*inclusion)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				continue
+			}
+			countLines(f, counts)
+			f.Close()
+		}
+		for line, inc := range counts {
+			if duplicateWithinFile(*inc) {
+				fmt.Printf("%d times:\t%s in ", inc.Count, line)
+				for k, v := range inc.Files {
+					if v > 1 {
+						fmt.Printf(" %v", k)
+					}
+				}
+				fmt.Println()
+			}
+		}
+	}
+}
+
+func duplicateWithinFile(inc inclusion) bool {
+	for _, v := range inc.Files {
+		if v > 1 {
+			return true
+		}
+	}
+	return false
 }
