9283bf0893b278b5a691501ad59b3aa3d76cbd1a
nobishino <nobishino@example.com>
2020-02-06 16:20:46 +0900

    Add 練習問題2.3, 2.4 Q: なぜloopだと遅くなるのか？

---
+++ ch02/popcount/popcount_test.go
@@ -0,0 +1,37 @@
+package popcount
+
+import (
+	"testing"
+)
+
+func BenchmarkPopCount(b *testing.B) {
+	for i := uint64(0); i < 1<<20; i++ {
+		PopCount(i)
+	}
+}
+func BenchmarkPopCount2_3(b *testing.B) {
+	for i := uint64(0); i < 1<<20; i++ {
+		PopCount2_3(i)
+	}
+}
+
+func BenchmarkPopCount2_4(b *testing.B) {
+	for i := uint64(0); i < 1<<20; i++ {
+		PopCount2_4(i)
+	}
+}
+
+func TestPopCount2_3(t *testing.T) {
+	for i := uint64(0); i < 1<<20; i++ {
+		if PopCount(i) != PopCount2_3(i) {
+			t.Fatalf("Popcount() returns %v, while PopCount2_3 returns %v", PopCount(i), PopCount2_3(i))
+		}
+	}
+}
+func TestPopCount2_4(t *testing.T) {
+	for i := uint64(0); i < 1<<20; i++ {
+		if PopCount(i) != PopCount2_4(i) {
+			t.Fatalf("Popcount() returns %v, while PopCount2_4 returns %v", PopCount(i), PopCount2_4(i))
+		}
+	}
+}
